ce7360d92194fa7bcb4b23e7288fb93ab00d55bc
Kim Corson <kimmc67b@gmail.com>
2021-12-06 11:45:13 -0500

    submitting controller update

---
+++ dotnet/Capstone/Controllers/AllowAnounomysAttribute.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Capstone.Controllers
+{
+    internal class AllowAnounomysAttribute : Attribute
+    {
+    }
+}
---
+++ dotnet/Capstone/Controllers/FlashCardController.cs
@@ -0,0 +1,33 @@
+﻿using Capstone.DAO;
+using Capstone.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Capstone.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FlashCardController : ControllerBase
+    {
+        private readonly IFlashCardDAO flashCardDAO;
+        public FlashCardController(IFlashCardDAO flashCardDAO)
+        {
+            this.flashCardDAO = flashCardDAO;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public ActionResult GetAllFlashCards()
+        {
+            IEnumerable<FlashCard> results = flashCardDAO.GetAllFlashCards();
+            return Ok(results);
+        }
+        
+
+    }
+}
