1634c62b103745673da7d4496e95d61ac5080b6b
Phillip Konkle <pkonkle213@gmail.com>
2021-12-08 12:12:27 -0500

    Good afternoon

--- vue/src/services/FlashCardService.js
+++ vue/src/services/FlashCardService.js
@@ -4,7 +4,6 @@ export default {
     fetchDeck(deckId) {
       return axios.get(`/FlashCard/${deckId}`)
     },
-<<<<<<< HEAD
 
     addNewCard(deckId, card) {
       return axios.post(`/FlashCard/deck/${deckId}`, card)
@@ -12,9 +11,5 @@ export default {
 
     addNewDeck() {
       return axios.post(``)
-=======
-    addCard(Card) {
-      return axios.post(`/FlashCard/deck/`,Card)
->>>>>>> 1c72a1a9a4042bf6c53fcc698268868ce80edee1
     }
 }
