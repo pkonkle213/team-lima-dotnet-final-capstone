8e6333eedfda3aee2af53676d3056efdc166a5d6
Kim Corson <kimmc67b@gmail.com>
2021-12-08 11:09:56 -0500

    submitting updates to DAO and flashcard controller status code results

--- dotnet/Capstone/DAO/DeckDAO.cs
+++ dotnet/Capstone/DAO/DeckDAO.cs
@@ -50,51 +50,5 @@ namespace Capstone.Models
             }
             return decks;
         }
-
-        // Considering a static class/method
-        /// <summary>
-        /// This method checks that the deck the user has selected will be connected to the current user's account
-        /// </summary>
-        /// <param name="userId"></param>
-        /// <param name="deckId"></param>
-        /// <returns></returns>
-        public bool UserHasAccessToDeck(int userId, int deckId)
-        {
-            List<Deck> decks = new List<Deck>();
-
-            const string sql = "SELECT deck_id, deck_name FROM decks WHERE deck_id = @deck_id AND user_id = @user_id";
-            using (SqlConnection conn = new SqlConnection(connectionString))
-            {
-                conn.Open();
-
-                using (SqlCommand command = new SqlCommand(sql, conn))
-                {
-                    command.Parameters.AddWithValue("@user_id", userId);
-                    command.Parameters.AddWithValue("@deck_id", deckId);
-
-                    using (SqlDataReader reader = command.ExecuteReader())
-                    {
-                        while (reader.Read())
-                        {
-                            Deck deck = new Deck();
-
-                            deck.Id = Convert.ToInt32(reader["deck_id"]);
-                            deck.Name = Convert.ToString(reader["deck_name"]);
-
-                            decks.Add(deck);
-                        }
-                    }
-                }
-            }
-
-            if (decks.Count == 0)
-            {
-                return false;
-            }
-            else
-            {
-                return true;
-            }
-        }
     }
 }
