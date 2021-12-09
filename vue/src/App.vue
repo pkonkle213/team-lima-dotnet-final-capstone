5116c05ad3955d553e8ad8f79236169a7ae7a3b4
Samuel Williams <samuelwilliams1357@gmail.com>
2021-12-09 09:44:20 -0500

    beginning custom navbar

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -2,56 +2,85 @@
      Note that you have classes from bootstrap available to you.
      See https://getbootstrap.com/docs/4.5/getting-started/introduction/ for reference on bootstrap -->
 <template>
-  <div id="app" class="container"> <!-- If you start to get random styling you don't like, remove container from this div -->
+  <div id="app">
+    <!-- If you start to get random styling you don't like, remove container from this div -->
     <div id="nav">
-      <router-link class="nav-item" v-bind:to="{ name: 'home' }">
-        <i class="fas fa-home"></i> <!-- This is a font awesome icon -->
-        Home 
-      </router-link>
       <router-link
         class="nav-item"
         v-bind:to="{ name: 'register' }"
-        v-if="!$store.state.token">&nbsp;|&nbsp;Register</router-link>
+        v-if="!$store.state.token"
+        >Register</router-link
+      >
       <router-link
         class="nav-item"
         v-bind:to="{ name: 'login' }"
-        v-if="!$store.state.token">
-        &nbsp;|&nbsp;Login
+        v-if="!$store.state.token"
+      >
+        Login
       </router-link>
       <router-link
         class="nav-item"
         v-bind:to="{ name: 'logout' }"
-        v-if="$store.state.token">
-        &nbsp;|&nbsp;Logout
+        v-if="$store.state.token"
+      >
+        Logout
       </router-link>
+      <h3>|</h3>
+      <router-link class="nav-item" v-bind:to="{ name: 'home' }"
+        >Home</router-link
+      >
+      <h3>|</h3>
       <router-link
         class="nav-item"
         v-bind:to="{ name: 'viewMyDecks' }"
-        v-if="$store.state.token">
-        &nbsp;|&nbsp; View My Decks
+        v-if="$store.state.token"
+      >
+        View My Decks
       </router-link>
-      
     </div>
-    
-    <router-view />
+
+    <router-view class="content" />
   </div>
 </template>
 
 <script>
-export default {
-  
-}
+export default {};
 </script>
 
 <!-- Application-Wide Styles go here. 
      Any valid CSS or SCSS is allowable here. See https://sass-lang.com/documentation/style-rules for info on SCSS -->
 <style lang="scss">
-
-@import './styles/colors.scss';
+@import "./styles/colors.scss";
 
 // Import the bootstrap styles. If you want to override Bootstrap colors, it needs to happen above this line
-@import '../node_modules/bootstrap/scss/bootstrap';
+@import "../node_modules/bootstrap/scss/bootstrap";
 
 // Your custom styles go below this point
 
+#nav {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 5%;
+  border-bottom: solid 20px blue;
+}
+
+.nav-item {
+  text-decoration: none;
+  font-size: 1.5rem;
+}
+
+h3 {
+  background-color: blue;
+  font-size: 2rem;
+  color: blue;
+  margin-bottom: 1px;
+  width: 20px;
+}
+
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
 </style>
