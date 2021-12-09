f329e10b3f03d845706bf3c912267690a0b92782
Phillip Konkle <pkonkle213@gmail.com>
2021-12-09 13:38:23 -0500

    Database update

--- dotnet/database/capstone.sql
+++ dotnet/database/capstone.sql
@@ -45,7 +45,7 @@ INSERT INTO Decks (user_id, deck_name,deck_description) VALUES (1, 'C Sharp','Tr
 INSERT INTO Decks (user_id, deck_name,deck_description) VALUES (1, 'D Flat','Jokes about how terms used for music can be translated into other forms');
 GO
 
-CREATE TABLE FlashCards (
+CREATE TABLE Flashcards (
 	flash_card_id int IDENTITY(1,1) NOT NULL,
 	front_text varchar(100) NOT NULL,
 	back_text varchar(100) NOT NULL,
@@ -54,9 +54,50 @@ CREATE TABLE FlashCards (
 	CONSTRAINT FK_created_by FOREIGN KEY (deck_id) references Decks (deck_id)
 )
 
-INSERT INTO FlashCards (front_text,back_text,deck_id) VALUES ('What is 1+1', '2', 1);
-INSERT INTO FlashCards (front_text,back_text,deck_id) VALUES ('What does immutable mean?','It cannot be changed', 3);
-INSERT INTO FlashCards (front_text,back_text,deck_id) VALUES ('What is 2+2', '4', 1);
-INSERT INTO FlashCards (front_text,back_text,deck_id) VALUES ('How can you loop through a list without an index','Foreach', 3);
+INSERT INTO Flashcards (front_text,back_text,deck_id) VALUES ('What is 1+1', '2', 1);
+INSERT INTO Flashcards (front_text,back_text,deck_id) VALUES ('What does immutable mean?','It cannot be changed', 3);
+INSERT INTO Flashcards (front_text,back_text,deck_id) VALUES ('What is 2+2', '4', 1);
+INSERT INTO Flashcards (front_text,back_text,deck_id) VALUES ('How can you loop through a list without an index','Foreach', 3);
 GO
 
+CREATE TABLE Tags (
+	tag_id int IDENTITY(1,1) NOT NULL,
+	name varchar(25) NOT NULL,
+
+	CONSTRAINT PK_Tags_tag_id PRIMARY KEY (tag_id)
+)
+
+INSERT INTO Tags (name) VALUES ('Math');
+INSERT INTO Tags (name) VALUES ('Computer');
+INSERT INTO Tags (name) VALUES ('Complicated');
+INSERT INTO Tags (name) VALUES ('EZPZ');
+INSERT INTO Tags (name) VALUES ('Phil made me');
+GO
+
+CREATE TABLE Flashcards_Tags (
+	flash_card_id int NOT NULL,
+	tag_id int NOT NULL
+	CONSTRAINT PK_Flashcards_Tags_flash_card_id_tag_id PRIMARY KEY (flash_card_id,tag_id),
+	CONSTRAINT FK_flash_card_id FOREIGN KEY (flash_card_id) REFERENCES Flashcards (flash_card_id),
+	CONSTRAINT FK_tag_id FOREIGN KEY (tag_id) REFERENCES Tags (tag_id),
+)
+
+INSERT INTO Flashcards_Tags (flash_card_id,tag_id) VALUES (1,1);
+INSERT INTO Flashcards_Tags (flash_card_id,tag_id) VALUES (1,4);
+INSERT INTO Flashcards_Tags (flash_card_id,tag_id) VALUES (1,5);
+INSERT INTO Flashcards_Tags (flash_card_id,tag_id) VALUES (3,3);
+INSERT INTO Flashcards_Tags (flash_card_id,tag_id) VALUES (3,2);
+INSERT INTO Flashcards_Tags (flash_card_id,tag_id) VALUES (2,2);
+INSERT INTO Flashcards_Tags (flash_card_id,tag_id) VALUES (2,4);
+INSERT INTO Flashcards_Tags (flash_card_id,tag_id) VALUES (2,5);
+INSERT INTO Flashcards_Tags (flash_card_id,tag_id) VALUES (4,5);
+GO
+
+/*
+SELECT * FROM Flashcards_tags
+
+SELECT *
+FROM Flashcards f
+INNER JOIN Flashcards_Tags ft ON ft.flash_card_id = f.flash_card_id
+INNER JOIN Tags t ON t.tag_id=ft.tag_id
+*/
