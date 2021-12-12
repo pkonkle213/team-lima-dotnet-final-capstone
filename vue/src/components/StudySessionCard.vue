542ed173a728ba8e5dbe7b9410a5c04a6b227dd2
Phillip Konkle <pkonkle213@gmail.com>
2021-12-12 15:09:01 -0500

    Getting closer to a session

--- vue/src/components/StudySessionCard.vue
+++ vue/src/components/StudySessionCard.vue
@@ -3,15 +3,15 @@
     <!-- Show the front if the card isn't answered   -->
     <div class="frontFace" v-if="showFront">
         <p>What is the airspeed velocity of an unladen swallow?</p>
-        <button class="button">Answer</button>
+        <button class="button" v-on:click.prevent="flipCard">Answer</button>
     </div>
 
     <!-- Once the button is clicked, the answer should show, the user should have
     the option of marking the card as right or wrong -->
     <div class="backSide" v-if="!showFront">
         <p>Did you get the answer right?</p>
-        <button>Yes</button>
-        <button>No</button>
+        <button v-on:click.prevent="rightAnswer">Yes</button>
+        <button v-on:click.prevent="wrongAnswer">No</button>
     </div>
 
     <!-- After selecting if right, store data for how many they got right (# and %)
@@ -26,6 +26,19 @@ export default {
             showFront: true,
 
         }
+    },
+    methods: {
+        flipCard() {
+            this.showFront=false;
+        },
+        rightAnswer() {
+            this.$store.state.numCorrect+=1;
+            console.log("Great job! You're so smart",this.$store.state.numCorrect);
+        },
+        wrongAnswer() {
+            this.$store.state.numIncorrect+=1;
+            console.log("Haha, you idiot.",this.$store.state.numIncorrect);
+        },
     }
 }
 </script>
