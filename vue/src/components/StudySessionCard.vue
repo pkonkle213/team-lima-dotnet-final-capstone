6264b4b5c01ff2ba7cf58839ad44c1dd571a492d
Phillip Konkle <pkonkle213@gmail.com>
2021-12-13 09:57:56 -0500

    Moar updates

--- vue/src/components/StudySessionCard.vue
+++ vue/src/components/StudySessionCard.vue
@@ -2,14 +2,14 @@
   <section class="studysession">
     <!-- Show the front if the card isn't answered   -->
     <div class="card">
-        <div v-if="showFront">
-            <p>{{$store.state.activeDeck[index].frontText}}</p>
+        <div v-if="showFront && !IsDone">
+            <p>Question {{index+1}}: {{$store.state.activeDeck[index].frontText}}</p>
             <button class="button" v-on:click.prevent="flipCard">Answer</button>
         </div>
 
         <!-- Once the button is clicked, the answer should show, the user should have
         the option of marking the card as right or wrong -->
-        <div v-if="!showFront">
+        <div v-if="!showFront && !IsDone">
             <p>Answer: {{$store.state.activeDeck[index].backText}}</p>
             <p>Did you get the answer right?</p>
             <!-- After selecting if right, store data for how many they got right (# and %)
@@ -17,9 +17,15 @@
             <button v-on:click.prevent="rightAnswer">Yes</button>
             <button v-on:click.prevent="wrongAnswer">No</button>
         </div>
+
+        <div v-if="IsDone">
+            <h4>Welcome to the end!</h4>
+            <p v-if="this.numCorrect>0">You got {{this.numCorrect}} question{{this.numCorrect===1 ? "" : "s" }} correct</p>
+            <p v-if="this.numIncorrect>0">You got {{this.numIncorrect}} question{{this.numIncorrect===1 ? "" : "s" }} incorrect</p>
+        </div>
     </div>
 
-    <button v-on:click.prevent="done">Would you like to tap out?</button>
+    <button v-on:click.prevent="done" v-if="!IsDone">Would you like to tap out?</button>
 
   </section>
 </template>
@@ -31,7 +37,8 @@ export default {
             showFront: true,
             index: 0,
             numCorrect: 0,
-            numIncorrect: 0
+            numIncorrect: 0,
+            IsDone: false,
         }
     },
     methods: {
@@ -55,11 +62,7 @@ export default {
             }
         },
         done() {
-            this.index=0;
-            console.log("Correct: ",this.numCorrect);
-            console.log("Incorrect: ",this.numIncorrect);
-            this.numCorrect=0,
-            this.numIncorrect=0
+            this.IsDone=true;
         }
     },
 }
