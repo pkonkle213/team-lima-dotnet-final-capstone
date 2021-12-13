7b514e4cb2e3e124816ad3b22e7f72f09c3329e3
Phillip Konkle <pkonkle213@gmail.com>
2021-12-13 16:14:36 -0500

    Major database update

--- dotnet/database/capstone.sql
+++ dotnet/database/capstone.sql
@@ -43,6 +43,9 @@ INSERT INTO Decks (user_id, deck_name,deck_description) VALUES (1, 'Math','Rando
 INSERT INTO Decks (user_id, deck_name,deck_description) VALUES (1, 'Engrish','Funny questions about Engrish');
 INSERT INTO Decks (user_id, deck_name,deck_description) VALUES (1, 'C Sharp','Trivia about a programming language');
 INSERT INTO Decks (user_id, deck_name,deck_description) VALUES (1, 'D Flat','Jokes about how terms used for music can be translated into other forms');
+INSERT INTO Decks (user_id, deck_name,deck_description) VALUES (1, '.NET - Module One','Questions from C#');
+INSERT INTO Decks (user_id, deck_name,deck_description) VALUES (1, '.NET - Module Two','Questions from SQL');
+INSERT INTO Decks (user_id, deck_name,deck_description) VALUES (1, '.NET - Module Three','Questions from HTML/CSS/Javascript');
 GO
 
 CREATE TABLE Flashcards (
@@ -56,8 +59,32 @@ CREATE TABLE Flashcards (
 
 INSERT INTO Flashcards (front_text,back_text,deck_id) VALUES ('What is 1+1', '2', 1);
 INSERT INTO Flashcards (front_text,back_text,deck_id) VALUES ('What does immutable mean?','It cannot be changed', 3);
-INSERT INTO Flashcards (front_text,back_text,deck_id) VALUES ('What is 2+2', '4', 1);
-INSERT INTO Flashcards (front_text,back_text,deck_id) VALUES ('How can you loop through a list without an index','Foreach', 3);
+INSERT INTO Flashcards (front_text,back_text,deck_id) VALUES ('What is 2+2?', '4', 1);
+INSERT INTO Flashcards (front_text,back_text,deck_id) VALUES ('How can you loop through a list without an index?','Foreach', 3);
+INSERT INTO Flashcards (front_text,back_text,deck_id) VALUES ('What primitive data types do you know in C#?','boolean, int, float, decimal, char...',5);
+INSERT INTO Flashcards (front_text,back_text,deck_id) VALUES ('What is a class?','The blueprint of an object',5);
+INSERT INTO Flashcards (front_text,back_text,deck_id) VALUES ('What''s the difference between a for loop and a while loop?','A for loop goes through a specific number of iterations and a while loop goes until a condition is met',5);
+INSERT INTO Flashcards (front_text,back_text,deck_id) VALUES ('What''s the difference between a while loop and a do while loop?','A do while loop will always run once then test the condition',5);
+INSERT INTO Flashcards (front_text,back_text,deck_id) VALUES ('What''s the difference between a stack and a queue?','Stack = FILO, Queue = FIFO',5);
+INSERT INTO Flashcards (front_text,back_text,deck_id) VALUES ('What is a unit test?','A test of a specific method (red,green,refactor)',5);
+INSERT INTO Flashcards (front_text,back_text,deck_id) VALUES ('What is immutability?','An object doesn''t inherantly change',5);
+INSERT INTO Flashcards (front_text,back_text,deck_id) VALUES ('What are the three parts of object oriented programming?','Encapsulation, Inheritance, Polymorphism',5);
+INSERT INTO Flashcards (front_text,back_text,deck_id) VALUES ('Is C# compiled or interpreted?','Compiled',5);
+INSERT INTO Flashcards (front_text,back_text,deck_id) VALUES ('What does the const keyword mean?','The value of the variable does not change',5);
+INSERT INTO Flashcards (front_text,back_text,deck_id) VALUES ('What git commands do you know?','Add, commit, pull, push',5);
+INSERT INTO Flashcards (front_text,back_text,deck_id) VALUES ('What’s the difference between inheriting a class and implementing an interface?','Inheritance is optional, an interface is mandatory',5);
+INSERT INTO Flashcards (front_text,back_text,deck_id) VALUES ('What is polymorphism?','Grouping things based upon their differences',5);
+INSERT INTO Flashcards (front_text,back_text,deck_id) VALUES ('What is the common language runtime?','virtual machine component of Microsoft .NET Framework, manages the execution of .NET programs',5);
+INSERT INTO Flashcards (front_text,back_text,deck_id) VALUES ('How many cans of Diet Doctor Pepper were consumed in or over Ohio last month?','',5);
+INSERT INTO Flashcards (front_text,back_text,deck_id) VALUES ('If you had an airplane that was completely full of jelly beans, how would you get the jelly beans out? You are allowed to only use boxes.','We leave this up to your imagination',5);
+INSERT INTO Flashcards (front_text,back_text,deck_id) VALUES ('What''s the difference between the heap and the queue','They''re not related?',5);
+INSERT INTO Flashcards (front_text,back_text,deck_id) VALUES ('What is a property in C#?','A member that provides a flexible mechanism to read, write, or compute the value of a private field',5);
+INSERT INTO Flashcards (front_text,back_text,deck_id) VALUES ('What does the virtual keyword mean?','the method and variable are marked as virtual in base class you can override in derived class and change the definition of that method',5);
+INSERT INTO Flashcards (front_text,back_text,deck_id) VALUES ('What access modifiers or visibilities do you know in C# and what do they mean?','Public - Access is not restricted, protected - Access is limited to the containing class or types derived from the containing class, internal - Access is limited to the current assembly, private - Access is limited to the containing type',5);
+INSERT INTO Flashcards (front_text,back_text,deck_id) VALUES ('What is an interface?','contains definitions for a group of related functionalities that a non-abstract class or a structure must implement',5);
+INSERT INTO Flashcards (front_text,back_text,deck_id) VALUES ('What is the difference between an array and a list','An arry is a specific list of items whose length cannot change, a list can be changed and manipulated',5);
+INSERT INTO Flashcards (front_text,back_text,deck_id) VALUES ('What’s the difference between C# and .NET?','.NET is the framework, C# is the language',5);
+INSERT INTO Flashcards (front_text,back_text,deck_id) VALUES ('What is a Dictionary?','A non-indexed, non-duplicated collection of items that have a key paired with a value',5);
 GO
 
 CREATE TABLE Tags (
